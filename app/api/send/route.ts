bc71098cc44c5a5f7241cdf804e8bfcf50d25848
Bernardo <bernardo_limas@hotmail.com>
2024-08-29 17:15:27 -0300

    testando rota de envio de email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -13,70 +13,71 @@ export async function POST(request: NextRequest) {
 
     if (!process.env.NEXT_TO_EMAIL_PERFORMATTI) throw new Error("Endereço de e-mail da Performatti não informado");
 
+    console.log(data)
     // Envio dos e-mails
-    await resend.emails.send({
-      from: `${process.env.NEXT_FROM_NAME} <${process.env.NEXT_FROM_EMAIL}>`,
-      to: [process.env.NEXT_TO_EMAIL_PERFORMATTI],
-      subject: 'Novo Contato Recebido - Performatti',
-      react: EmailToPerformatti({
-        name: data.name,
-        phone: data.phone,
-        message: data.message,
-        company: data.company,
-        corporativeEmail: data.email,
-        userFirstName: data.name.split(' ')[0],
-        fieldActivity: data.fieldActivity,
-        collaboratorsNumber: data.collaboratorsNumber,
-      }),
-    });
+    // await resend.emails.send({
+    //   from: `${process.env.NEXT_FROM_NAME} <${process.env.NEXT_FROM_EMAIL}>`,
+    //   to: [process.env.NEXT_TO_EMAIL_PERFORMATTI],
+    //   subject: 'Novo Contato Recebido - Performatti',
+    //   react: EmailToPerformatti({
+    //     name: data.name,
+    //     phone: data.phone,
+    //     message: data.message,
+    //     company: data.company,
+    //     corporativeEmail: data.email,
+    //     userFirstName: data.name.split(' ')[0],
+    //     fieldActivity: data.fieldActivity,
+    //     collaboratorsNumber: data.collaboratorsNumber,
+    //   }),
+    // });
 
-    await resend.emails.send({
-      from: `${process.env.NEXT_FROM_NAME} <${process.env.NEXT_FROM_EMAIL}>`,
-      to: [data.email],
-      subject: 'Obrigado por Entrar em Contato - Performatti',
-      react: EmailToUser({
-        userFirstName: data.name.split(' ')[0],
-      }),
-    });
+    // await resend.emails.send({
+    //   from: `${process.env.NEXT_FROM_NAME} <${process.env.NEXT_FROM_EMAIL}>`,
+    //   to: [data.email],
+    //   subject: 'Obrigado por Entrar em Contato - Performatti',
+    //   react: EmailToUser({
+    //     userFirstName: data.name.split(' ')[0],
+    //   }),
+    // });
 
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_CLIENT_KEY?.replace(/\\n/g, '\n'),
-      },
-      scopes: [
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.file',
-        'https://www.googleapis.com/auth/spreadsheets'
-      ]
-    })
+    // const auth = new google.auth.GoogleAuth({
+    //   credentials: {
+    //     client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    //     private_key: process.env.GOOGLE_CLIENT_KEY?.replace(/\\n/g, '\n'),
+    //   },
+    //   scopes: [
+    //     'https://www.googleapis.com/auth/drive',
+    //     'https://www.googleapis.com/auth/drive.file',
+    //     'https://www.googleapis.com/auth/spreadsheets'
+    //   ]
+    // })
 
-    const sheets = google.sheets({
-      auth,
-      version: 'v4'
-    })
+    // const sheets = google.sheets({
+    //   auth,
+    //   version: 'v4'
+    // })
 
-    const newDateFormatted = format(new Date(), 'dd-MM-yyyy');
+    // const newDateFormatted = format(new Date(), 'dd-MM-yyyy');
 
-    await sheets.spreadsheets.values.append({
-      spreadsheetId: process.env.GOOGLE_SHEET_ID,
-      range: "A1:H1",
-      valueInputOption: "USER_ENTERED",
-      requestBody: {
-        values: [
-          [
-            data.name,
-            data.email,
-            data.phone,
-            data.company,
-            data.fieldActivity,
-            data.collaboratorsNumber,
-            data.message,
-            newDateFormatted
-          ]
-        ]
-      }
-    })
+    // await sheets.spreadsheets.values.append({
+    //   spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    //   range: "A1:H1",
+    //   valueInputOption: "USER_ENTERED",
+    //   requestBody: {
+    //     values: [
+    //       [
+    //         data.name,
+    //         data.email,
+    //         data.phone,
+    //         data.company,
+    //         data.fieldActivity,
+    //         data.collaboratorsNumber,
+    //         data.message,
+    //         newDateFormatted
+    //       ]
+    //     ]
+    //   }
+    // })
 
     return NextResponse.json({ message: 'E-mail enviado com sucesso e planilha atualizada!' });
   } catch (error) {
